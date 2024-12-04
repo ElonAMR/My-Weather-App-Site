c24b512905025522b79425c8fa200a36e5df28e4
Elon Amram <136482377+ElonAMR@users.noreply.github.com>
2024-12-04 20:47:26 +0200

    Weather Data Fetch Function
    This function fetches weather data asynchronously from the API and updates the weather display. If there is an error, it logs the error to the console.

--- WeatherApp.js
+++ WeatherApp.js
@@ -1,3 +1,24 @@
 const apiKey = "YOUR_API_KEY";
 let city = "YOUR_LOCATION_DEFAULT";
 let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+
+
+let weatherData={};
+
+async function fetchWeatherData(){
+    try{
+        const response = await fetch(url);
+
+        if (!response.ok){
+            throw new Error("Problem receiving data");
+        }
+
+        weatherData = await response.json();
+        console.log(weatherData);
+        updateWeatherDisplay(weatherData);
+    }
+    catch (e){
+        console.log(e);
+        throw e;
+    }
+}
